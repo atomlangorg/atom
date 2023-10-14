f2e2b6bad800d4462ec20c3c9a268f50ff9fe9d4
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2023-10-14 23:25:31 +0100

    Do initial setup (#1)

--- Sources/Atom.swift
+++ Sources/Atom.swift
@@ -1,14 +1,8 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-// 
-// Swift Argument Parser
-// https://swiftpackageindex.com/apple/swift-argument-parser/documentation
-
 import ArgumentParser
 
 @main
 struct Atom: ParsableCommand {
-    mutating func run() throws {
-        print("Hello, world!")
+    func run() {
+        print("atom")
     }
 }
