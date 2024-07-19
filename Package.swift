893f632b4437c2581037aef543d71182495e2300
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-07-19 01:35:47 +0100

    Update swift-argument-parser dependency

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v14),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
     ],
     targets: [
         .executableTarget(
