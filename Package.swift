8095c8a5b55d044dc58ad7424b6b80495534bb0e
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-07-19 01:30:40 +0100

    Support macOS

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "atom",
+    platforms: [
+        .macOS(.v14),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
     ],
