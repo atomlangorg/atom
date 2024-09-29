763b029de8117021eee3e76741c049a754b9cabb
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-29 15:16:27 +0100

    Add other addition tests

--- Tests/AtomTests/PrecedenceTests.swift
+++ Tests/AtomTests/PrecedenceTests.swift
@@ -10,6 +10,38 @@ import Testing
 
 @Suite("Precedence")
 struct PrecedenceTests {
+    @Test("Check precedence of one addition")
+    func oneAddition() {
+        let input = "3 + 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "(3 + 3)")
+    }
+
+    @Test("Check precedence of two additions")
+    func twoAdditions() {
+        let input = "3 + 3 + 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "((3 + 3) + 3)")
+    }
+
     @Test("Check precedence of three additions")
     func threeAdditions() {
         let input = "3 + 3 + 3 + 3"
