039d88aca878efbe8e343f93c213791a4895c64a
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-29 15:20:38 +0100

    Add multiplication tests

--- Tests/AtomTests/PrecedenceTests.swift
+++ Tests/AtomTests/PrecedenceTests.swift
@@ -57,4 +57,52 @@ struct PrecedenceTests {
 
         #expect(ir.swift() == "(((3 + 3) + 3) + 3)")
     }
+
+    @Test("Check precedence of one multiplication")
+    func oneMultiplication() {
+        let input = "3 * 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "(3 * 3)")
+    }
+
+    @Test("Check precedence of two multiplications")
+    func twoMultiplications() {
+        let input = "3 * 3 * 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "((3 * 3) * 3)")
+    }
+
+    @Test("Check precedence of three multiplications")
+    func threeMultiplications() {
+        let input = "3 * 3 * 3 * 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "(((3 * 3) * 3) * 3)")
+    }
 }
