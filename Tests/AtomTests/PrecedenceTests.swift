356050ad9f53a9f49f774b82fbdbcfa6ea27efea
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-29 17:16:54 +0100

    Add tests for a mix of addition and multiplication

--- Tests/AtomTests/PrecedenceTests.swift
+++ Tests/AtomTests/PrecedenceTests.swift
@@ -105,4 +105,68 @@ struct PrecedenceTests {
 
         #expect(ir.swift() == "(((3 * 3) * 3) * 3)")
     }
+
+    @Test("Check precedence for a mix of additions and multiplications (1)")
+    func additionMultiplicationMix1() {
+        let input = "3 + 3 * 3 + 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "((3 + (3 * 3)) + 3)")
+    }
+
+    @Test("Check precedence for a mix of additions and multiplications (2)")
+    func additionMultiplicationMix2() {
+        let input = "3 + 3 + 3 * 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "((3 + 3) + (3 * 3))")
+    }
+
+    @Test("Check precedence for a mix of additions and multiplications (3)")
+    func additionMultiplicationMix3() {
+        let input = "3 * 3 + 3 + 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "(((3 * 3) + 3) + 3)")
+    }
+
+    @Test("Check precedence for a mix of additions and multiplications (4)")
+    func additionMultiplicationMix4() {
+        let input = "3 * 3 + 3 * 3"
+        var stream = Stream(string: input)
+        let result = IntegerExpr.consume(stream: &stream, context: GrammarContext())
+
+        #expect(stream.isEnd())
+
+        guard case let .doConsume(ir) = result else {
+            Issue.record("Result was not consumed")
+            return
+        }
+
+        #expect(ir.swift() == "((3 * 3) + (3 * 3))")
+    }
 }
