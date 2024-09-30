da1736f27f44100aa32ad58b3a076226c70e82d3
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-09-30 22:45:09 +0100

    Add remaining lowercase letters

--- Sources/Bootstrap/Grammar/Literal.swift
+++ Sources/Bootstrap/Grammar/Literal.swift
@@ -70,16 +70,96 @@ enum Literal {
         static let literal: Character = "="
     }
 
+    enum LowercaseA: GrammarLiteral {
+        static let literal: Character = "a"
+    }
+
+    enum LowercaseB: GrammarLiteral {
+        static let literal: Character = "b"
+    }
+
+    enum LowercaseC: GrammarLiteral {
+        static let literal: Character = "c"
+    }
+
+    enum LowercaseD: GrammarLiteral {
+        static let literal: Character = "d"
+    }
+
     enum LowercaseE: GrammarLiteral {
         static let literal: Character = "e"
+    }
+
+    enum LowercaseF: GrammarLiteral {
+        static let literal: Character = "f"
+    }
+
+    enum LowercaseG: GrammarLiteral {
+        static let literal: Character = "g"
+    }
+
+    enum LowercaseH: GrammarLiteral {
+        static let literal: Character = "h"
+    }
+
+    enum LowercaseI: GrammarLiteral {
+        static let literal: Character = "i"
+    }
+
+    enum LowercaseJ: GrammarLiteral {
+        static let literal: Character = "j"
+    }
+
+    enum LowercaseK: GrammarLiteral {
+        static let literal: Character = "k"
     }
 
     enum LowercaseL: GrammarLiteral {
         static let literal: Character = "l"
     }
 
+    enum LowercaseM: GrammarLiteral {
+        static let literal: Character = "m"
+    }
+
+    enum LowercaseN: GrammarLiteral {
+        static let literal: Character = "n"
+    }
+
+    enum LowercaseO: GrammarLiteral {
+        static let literal: Character = "o"
+    }
+
+    enum LowercaseP: GrammarLiteral {
+        static let literal: Character = "p"
+    }
+
+    enum LowercaseQ: GrammarLiteral {
+        static let literal: Character = "q"
+    }
+
+    enum LowercaseR: GrammarLiteral {
+        static let literal: Character = "r"
+    }
+
+    enum LowercaseS: GrammarLiteral {
+        static let literal: Character = "s"
+    }
+
     enum LowercaseT: GrammarLiteral {
         static let literal: Character = "t"
+    }
+
+    enum LowercaseU: GrammarLiteral {
+        static let literal: Character = "u"
+    }
+
+    enum LowercaseV: GrammarLiteral {
+        static let literal: Character = "v"
+    }
+
+    enum LowercaseW: GrammarLiteral {
+        static let literal: Character = "w"
     }
 
     enum LowercaseX: GrammarLiteral {
