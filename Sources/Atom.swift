b1a75ce292b45dceeff0bdcbaa12d4850554b3b4
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-10-05 18:04:32 +0100

    Add line feed to string

--- Sources/Atom.swift
+++ Sources/Atom.swift
@@ -5,7 +5,7 @@ struct Atom: ParsableCommand {
     func run() {
         print("atom")
 
-        let input = #"let x = 3 + 3 * 3 + 3\#nlet greeting = "hello world""#
+        let input = #"let x = 3 + 3 * 3 + 3\#nlet greeting = "hello\nworld""#
         var stream = Stream(string: input)
         let result = Match.Program.consume(stream: &stream, context: GrammarContext())
         print("result =", result)
