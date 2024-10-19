9fdd12dfc8039b75d40400fa29f46e46c07712e1
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2024-10-19 14:06:03 +0100

    Format input code

--- Sources/Atom.swift
+++ Sources/Atom.swift
@@ -5,7 +5,10 @@ struct Atom: ParsableCommand {
     func run() {
         print("atom")
 
-        let input = #"let x = 3 + 3 * 3 + 3\#nlet greeting = "hello\nworld""#
+        let input = #"""
+let x = 3 + 3 * 3 + 3
+let greeting = "hello\nworld"
+"""#
         var stream = Stream(string: input)
         let result = Match.Program.consume(stream: &stream, context: GrammarContext())
 
