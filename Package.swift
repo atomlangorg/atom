fe1899c906f1cf031c313f124d38336bad69a4b9
George Elsham <40073010+GeorgeElsham@users.noreply.github.com>
2025-01-12 19:03:38 +0000

    Change requirement to macOS 15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "atom",
     platforms: [
-        .macOS(.v14),
+        .macOS(.v15),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
